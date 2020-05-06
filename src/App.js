ed7dcb54e651d7a6bf13f4637d51ec663c856b12
Sergey Barbolin <sergeybarbolin25@gmail.com>
2020-05-06 18:53:59 +0300

    ''

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title is-1">sharecount</h1>
+    </div>
+  );
+}
+
+export default App;
